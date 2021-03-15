0c7849831db18dff0ba92e88b11cebe35cd2e0c0
J/A <archer884@gmail.com>
2021-03-15 14:10:09 -0500

    wip

---
+++ src/characterization.rs
@@ -0,0 +1,1 @@
+// todo: write file characterization code
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,60 @@
-use std::env;
-use std::io::{self, Write};
+// FIXME: add file type and extension filters
+// Maybe also a regex/string.contains() filter
+
+use std::{env, error, fmt::Display, io::{self, Write}, str::FromStr};
+
+mod characterization;
+
+use clap::Clap;
 use walkdir::WalkDir;
 
 type Result<T = (), E = Box<dyn std::error::Error + 'static>> = std::result::Result<T, E>;
 
+#[derive(Clap, Clone, Debug)]
+struct Opts {
+    #[clap(short, long)]
+    extension: Option<String>,
+    #[clap(short, long)]
+    kind: Option<FileKind>,
+    #[clap(short, long)]
+    contains: Option<String>,
+}
+
+#[derive(Clone, Debug)]
+enum FileKind {
+    Image,
+    Text,
+    Video,
+}
+
+impl FromStr for FileKind {
+    type Err = ParseFileKindError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let kind = s.to_ascii_lowercase();
+        match &*kind {
+            "i" | "image" => Ok(FileKind::Image),
+            "t" | "text" => Ok(FileKind::Text),
+            "v" | "video" => Ok(FileKind::Video),
+            _ => Err(ParseFileKindError),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+struct ParseFileKindError;
+
+impl Display for ParseFileKindError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("use image, text, or video")
+    }
+}
+
+impl error::Error for ParseFileKindError {}
+
 fn main() -> Result {
+    let opts = Opts::parse();
+    
     let cwd = env::current_dir()?;
     let paths = WalkDir::new(&cwd).into_iter().filter_entry(|entry| {
         entry
